d8e90730a27b9683fb5111180e33c200fbb41751
Mark Nottingham <mnot@mnot.net>
2025-11-26 13:57:44 +1100

    bump version

--- http_sf/__init__.py
+++ http_sf/__init__.py
@@ -25,7 +25,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 
 from typing import Tuple, List, Dict, Optional
 
